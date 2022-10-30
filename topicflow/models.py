d43ed6e5e76128895d1cbbe1951ad4b5163d7d73
JannisZeller <jannis.zeller@arcor.de>
2022-10-30 14:00:44 +0100

    Minor changes

--- topicflow/models.py
+++ topicflow/models.py
@@ -2,5 +2,33 @@
 # topicflow.models
 # ----------------
 # 
-# Models in topicflow.
+# LDA Model.
 # ------------------------------------------------------------------------------
+
+
+# %% Dependencies
+# ------------------------------------------------------------------------------
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+
+
+# %% LDA Model
+# ------------------------------------------------------------------------------
+class LDA(tf.keras.Model):
+    def __init__(self, num_classes=1000):
+        super(LDA, self).__init__()
+        pass
+
+    def call(self, inputs):
+        pass
+
+
+
+
+# %% Main
+# ------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    print("Test")
